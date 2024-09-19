2e8db036aff8518576ecdccd73166842eadf9c50
XinNie <umnie2@myumanitoba.ca>
2024-09-19 14:53:16 -0500

    my category practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+function SearchBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <input type="text" placeholder="Search..." />
+        <div>
+          <input type="checkbox" />
+          Only show products in stock
+        </div>
+      </form>
+    </>
   );
 }
 
-export default App;
+function ProductTable() {}
+
+function ProductCategoryRow() {}
+
+function ProductRow() {}
+
+export default function FilterProductTable() {
+  return <SearchBar></SearchBar>;
+}
