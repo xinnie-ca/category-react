ed6afb5f81b8ddb9fe9840fd63f49eeee0783249
XinNie <umnie2@myumanitoba.ca>
2024-09-19 16:01:55 -0500

    working list

--- src/App.js
+++ src/App.js
@@ -6,20 +6,92 @@ function SearchBar() {
       <form>
         <input type="text" placeholder="Search..." />
         <div>
-          <input type="checkbox" />
-          Only show products in stock
+          <input type="checkbox" /> Only show products in stock
         </div>
       </form>
     </>
   );
 }
 
-function ProductTable() {}
+function ProductRow({ product }) {
+  // This is a function called name, need pass product as parameter.
+  const name = (product) => {
+    if (product.stocked) {
+      return product.name;
+    } else {
+      return <span style={{ color: "red" }}>{product.name}</span>;
+    }
+  };
+  return (
+    <tr>
+      <td>{name(product)}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
 
-function ProductCategoryRow() {}
+function ProductCategoryRow({ category }) {
+  return (
+    <>
+      <tr col>
+        <th>{category}</th>
+      </tr>
+    </>
+  );
+}
 
-function ProductRow() {}
+function ProductTable({ products }) {
+  // rows contains JSX
+  const rows = [];
+  let lastCategory = null;
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name}></ProductRow>);
+    lastCategory = product.category;
+  });
 
-export default function FilterProductTable() {
-  return <SearchBar></SearchBar>;
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+
+        <tbody>{rows}</tbody>
+      </table>
+    </>
+  );
+}
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
+function FilterableProductTable() {
+  return (
+    <>
+      <div>
+        <SearchBar></SearchBar>
+        <ProductTable products={PRODUCTS} />
+      </div>
+    </>
+  );
+}
+export default function App() {
+  return <FilterableProductTable products={PRODUCTS} />;
 }
